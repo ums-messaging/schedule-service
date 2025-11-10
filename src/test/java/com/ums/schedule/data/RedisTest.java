2eb8d1acf6adf3bef4620fe70d6388c2f3ca1bb5
jang314 <jang314@naver.com>
2025-11-10 12:16:46 +0900

    UMSMSA-70 : feat(schedule-service) - Prometheus 연동

---
+++ src/test/java/com/ums/schedule/data/RedisTest.java
@@ -0,0 +1,11 @@
+package com.ums.schedule.data;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+public class RedisTest {
+
+}
