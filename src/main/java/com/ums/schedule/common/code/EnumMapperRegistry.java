7637528c725b153bb9b219c7ed561800feaf082e
jang314 <jang314@naver.com>
2026-03-18 15:14:34 +0900

    UMSMSA-257 : feat(schedule-service) - 코드 정의

--- src/main/java/com/ums/schedule/common/code/EnumMapperRegistry.java
+++ src/main/java/com/ums/schedule/common/code/EnumMapperRegistry.java
@@ -1,5 +1,6 @@
 package com.ums.schedule.common.code;
 
+import com.ums.schedule.message.code.EmailMessageEnumMapper;
 import com.ums.schedule.template.domain.code.TemplateEnumMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,5 +10,6 @@ public class EnumMapperRegistry {
     @Autowired
     public EnumMapperRegistry(EnumMapperFactory factory) {
         factory.register(TemplateEnumMapper.class);
+        factory.register(EmailMessageEnumMapper.class);
     }
 }
