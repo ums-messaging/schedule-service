46c7b264d1ce300f4113f69311f4aa750db5a801
jang314 <jang314@naver.com>
2026-03-20 11:16:43 +0900

    UMSMSA-256 : feat(schedule-service) - Attachment 관련 코드 추가

--- src/main/java/com/ums/schedule/common/code/EnumMapperRegistry.java
+++ src/main/java/com/ums/schedule/common/code/EnumMapperRegistry.java
@@ -1,5 +1,6 @@
 package com.ums.schedule.common.code;
 
+import com.ums.schedule.attachment.code.AttachmentEnumMapper;
 import com.ums.schedule.message.code.EmailMessageEnumMapper;
 import com.ums.schedule.template.domain.code.TemplateEnumMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,5 +12,6 @@ public class EnumMapperRegistry {
     public EnumMapperRegistry(EnumMapperFactory factory) {
         factory.register(TemplateEnumMapper.class);
         factory.register(EmailMessageEnumMapper.class);
+        factory.register(AttachmentEnumMapper.class);
     }
 }
